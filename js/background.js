a3b03ca1e62c286003ec12b253dd5e881fc72cab
Justin Ho <justinho515@gmail.com>
2019-06-13 08:03:39 -0700

    Fix bug that reset all time variables to 0 at midnight, even when it wasn't time to reset

--- js/background.js
+++ js/background.js
@@ -1,17 +1,14 @@
-// Declare global time variables.
-let now, day, week, month, year;
-
 // Set up environment on extension installation.
-chrome.runtime.onInstalled.addListener( 
+chrome.runtime.onInstalled.addListener(
     function installationSetUp() {
-        // console.log('Starting initializations ...') ;
+        console.log('Starting initializations ...') ;
         // Set extension installation time data.
-        now = new Date();
-        day = now.getDay();
-        let result = getWeekNumber(now);
-        week = result[1];
-        month = now.getMonth();
-        year = now.getFullYear();
+        let now = new Date();
+        let currDay = now.getDay();
+        let currResult = getWeekNumber(now);
+        let currWeek = result[1];
+        let currMonth = now.getMonth();
+        let currYear = now.getFullYear();
 
         // Get offset between current time and the next midnight.
         let then = new Date(now);
@@ -24,25 +21,27 @@ chrome.runtime.onInstalled.addListener(
         chrome.storage.sync.get(['todaySec', 'weekSec', 'monthSec', 'yearSec', 'alltimeSec'], (result) => {
             // console.log('todaySec = ' + result.todaySec);
             // console.log('typeOf todaySec = ' + typeof result.todaySec);
+
+            // Set time variables if they don't already exist in Chrome sync storage.
             if (typeof result.todaySec == 'undefined') chrome.storage.sync.set({'todaySec': 0});
             if (typeof result.weekSec == 'undefined') chrome.storage.sync.set({'weekSec': 0});
             if (typeof result.monthSec == 'undefined') chrome.storage.sync.set({'monthSec': 0});
             if (typeof result.yearSec == 'undefined') chrome.storage.sync.set({'yearSec': 0});
             if (typeof result.alltimeSec == 'undefined') chrome.storage.sync.set({'alltimeSec': 0});
+
+            // Store current time period's details in Chrome sync storage.
+            chrome.storage.sync.set({'currDay': currDay, 'currWeek': currWeek, 'currMonth': currMonth, 'currYear': currYear}, () => {
+                console.log('Current time period time vars set');
+            });
         });
-        // chrome.storage.sync.set({'todaySec': 0, 'weekSec': 0, 'monthSec': 0, 'yearSec': 0, 'alltimeSec': 0,
-        //             'prevDaySec': 0, 'prevWeekSec': 0, 'prevMonthSec': 0, 'prevYearSec': 0,
-        //             'yearInstall': year, 'monthInstall': month, 'weekInstall': week, 'dayInstall': day}, () => {
-        //     // console.log('Time variables and installation variables have been created and stored.');
-        // });
 
         // Create alarm to tell background script to refresh the appropriate time variables at midnight each day.
-        // console.log('Creating alarm ...');    
+        console.log('Creating alarm ...');
         chrome.alarms.create('resetTimeVars', {delayInMinutes: minUntilMidnight, periodInMinutes: 60 * 24});
 
         // For testing & debugging purposes.
         // chrome.alarms.create('resetTimeVars', {delayInMinutes: 1, periodInMinutes: 1});
-        // console.log('Alarm created');
+        console.log('Alarm created');
 
         // Remove onInstalled listener to prevent resetting of time variables upon browser update.
         chrome.runtime.onInstalled.removeListener(installationSetUp);
@@ -54,24 +53,26 @@ chrome.alarms.onAlarm.addListener( (alarm) => {
     switch (alarm.name) {
         case 'resetTimeVars':
             // Reset the appropriate time variables at midnight of each day
-            // console.log('RESETTING TIME VARS!!!');
-            let d = new Date();
+            console.log('RESETTING TIME VARS!!!');
+            let today = new Date();
             chrome.storage.sync.get(['todaySec', 'weekSec', 'monthSec', 'yearSec', 'alltimeSec',
+                'currDay', 'currWeek', 'currMonth', 'currYear',
                 'prevDaySec', 'prevWeekSec', 'prevMonthSec', 'prevYearSec'], (result) => {
+                console.log('Resseting day time ...');
                 chrome.storage.sync.set({'prevDaySec': result.todaySec, 'todaySec': 0});
-                let weekResult = getWeekNumber(new Date());
-                if (weekResult[1] != result.week) {
-                    chrome.storage.sync.set({'prevWeekSec': result.weekSec, 'weekSec': 0});                    
-                    week = weekResult[1];
+                let weekResult = getWeekNumber(today);
+                if (weekResult[1] != result.currWeek) {
+                    console.log('Resetting week time ...');
+                    chrome.storage.sync.set({'prevWeekSec': result.weekSec, 'currWeek': weekResult[1], 'weekSec': 0});                    
                 }
-                if (d.getMonth() != result.month) {
-                    chrome.storage.sync.set({'prevMonthSec': result.monthSec, 'monthSec': 0});                    
-                    month = d.getMonth();
+                if (today.getMonth() != result.currMonth) {
+                    console.log('Resetting month time ...');
+                    chrome.storage.sync.set({'prevMonthSec': result.monthSec, 'currMonth': today.getMonth(), 'monthSec': 0});                    
                 }
-                if (d.getFullYear() != result.year) {
-                    chrome.storage.sync.set({'prevYearSec': result.yearSec, 'yearSec': 0});                    
-                    year++;
-                }                
+                if (today.getFullYear() != result.currYear) {
+                    console.group('Resetting year time ...');
+                    chrome.storage.sync.set({'prevYearSec': result.yearSec, 'currYear': today.getFullYear(), 'yearSec': 0});
+                }
             });
             break;
         default:
@@ -103,16 +104,16 @@ chrome.alarms.onAlarm.addListener( (alarm) => {
  * e.g. 2014/12/29 is Monday in week  1 of 2015
  *      2012/1/1   is Sunday in week 52 of 2011
  */
-function getWeekNumber(d) {
+function getWeekNumber(day) {
     // Copy date so don't modify original
-    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    day = new Date(Date.UTC(day.getFullYear(), day.getMonth(), day.getDate()));
     // Set to nearest Thursday: current date + 4 - current day number
     // Make Sunday's day number 7
-    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    day.setUTCDate(day.getUTCDate() + 4 - (day.getUTCDay() || 7));
     // Get first day of year
-    let yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0,1));
+    let yearStart = new Date(Date.UTC(day.getUTCFullYear(), 0, 1));
     // Calculate full weeks to nearest Thursday
-    let weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1) / 7);
+    let weekNo = Math.ceil((( (day - yearStart) / 86400000) + 1) / 7);
     // Return array of year and week number
     return [d.getUTCFullYear(), weekNo];
 }
